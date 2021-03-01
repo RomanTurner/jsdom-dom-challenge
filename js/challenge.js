b3e384a303f5ca99a9a414861e0e7f913bf31955
Roman Turner <rn.turner@live.com>
2021-03-01 00:05:08 -0800

    assignment complete

--- js/challenge.js
+++ js/challenge.js
@@ -1,56 +1,79 @@
-let counterBox = document.getElementById('countera');
-let plusBtn = document
-  .getElementById("plus")
-  .addEventListener("click", increment);
-let heartBtn = document
-  .getElementById("heart")
-  .addEventListener("click", likeCounter);
-let minusBtn = document
-  .getElementById("minus")
-  .addEventListener("click", decrement);
-let pauseBtn = document.getElementById('pause');
-let likeList = document.querySelector('.likes');
-let counter = +counterBox.innerText;
+const counterBox = document.getElementById("counter");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const heartBtn = document.getElementById("heart");
+const pauseBtn = document.getElementById("pause");
+const likes = document.getElementsByClassName("likes");
+const commentList = document.getElementById("list");
+const submitBtn = document.getElementById("submit");
+let li = document.createElement("li");
+let p = document.createElement("p");
 
-//Setting counter to increment each second
-setInterval(function () {
-    counterBox.innerText = +counterBox.innerText + 1;
-    counter += 1;
-}, 1000);
+let counter = 0;
+let numbers = [];
+let pauseBool = false;
 
-//increment by 1 per click
+//listeners
+submitBtn.addEventListener("click", submitHandler);
+pauseBtn.addEventListener("click", pauseHandler);
+heartBtn.addEventListener("click", likeHandler);
+minusBtn.addEventListener("click", decrement);
+plusBtn.addEventListener("click", increment);
+
+//counter goes up every second.
+document.addEventListener("DOMContentLoaded", startCounter);
+//starts the counter
+function startCounter() {
+  pauseBool === false
+    ? (counter = setInterval(increment, 1000))
+    : clearInterval(counter);
+}
+//increments counter by 1
 function increment() {
-    counterBox.innerText = +counterBox.innerText + 1;
-    counter += 1;
-};
-//decrement by 1 per click
+  counterBox.innerText = +counterBox.innerHTML + 1;
+}
+//decreases counter by 1
 function decrement() {
-    counterBox.innerText = +counterBox.innerText - 1;
-    counter -= 1;
-};
-
-
-//appends likes, and updates the amount
-let dataTracker = counterBox.innerText;
-let likeAmount = 2;
-
-function likeCounter() {
-    let liLike = document.createElement("li");
-    liLike.setAttribute("id", counter);
-    let likeArr = [];
-    let likeId = document.getElementById(counter)
-    console.log(likeId);
- 
-        if (likeArr.includes(likeId)) {
-          likeList.lastChild.innerText =
-            counter + " has been liked " + likeAmount + " times.";
-          likeAmount += 1;
-        } else {
-          liLike.innerText = counter + " has been liked 1 time.";
-          likeList.appendChild(liLike);
-          likeAmount = 2;
-          dataTracker = counter;
-        }
-};
-
-
+  counterBox.innerText = +counterBox.innerText - 1;
+}
+//captures likes, appends to ul
+function likeHandler() {
+  let number = counterBox.innerHTML;
+  let likeCounter = 0;
+  numbers.push(number);
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] == number) {
+      likeCounter++;
+    }
+  }
+  likes[0].append(li);
+  likes[0].children[0].innerHTML = `${number} has been liked ${likeCounter} times`;
+}
+//pauses functionality
+function pauseHandler() {
+  if (pauseBool === false) {
+    disabler(true);
+    startCounter();
+    pauseBtn.innerHTML = "resume";
+  } else {
+    disabler(false);
+    startCounter();
+    pauseBtn.innerHTML = "pause";
+  }
+  console.log(pauseBool);
+}
+//disables buttons
+function disabler(x) {
+  pauseBool = x;
+  plusBtn.disabled = x;
+  submitBtn.disabled = x;
+  minusBtn.disabled = x;
+  heartBtn.disabled = x;
+}
+//collects input, appends to comment section
+function submitHandler(e) {
+  e.preventDefault();
+  p.innerHTML = document.getElementById("comment-input").value;
+  commentList.appendChild(p);
+  document.getElementById("comment-input").value = "";
+}
